36090502bd68cb54780ec93a700745f2594d3431
Brandon Ip <Chupalika@gmail.com>
2018-04-19 14:35:12 -0400

    team colors, fixed dots flashing in and out bug, etc.
    - dots are bordered green or red depending on team
    - dots no longer disappear if they haven't moved
    - dots become squares when player HP is 0
    - changed the color scheme of the dots

--- scatterchart.js
+++ scatterchart.js
@@ -1,5 +1,9 @@
 var dataset = [];
 var currentsecond = 0;
+var dotsize = 10;
+var legendsize = 10;
+//var colors = [d3.rgb("#9e0142"), d3.rgb("#d53e4f"), d3.rgb("#f46d43"), d3.rgb("#fdae61"), d3.rgb("#fee08b"), d3.rgb("#e6f598"), d3.rgb("#abdda4"), d3.rgb("#66c2a5"), d3.rgb("#3288bd"), d3.rgb("#5e4fa2")];
+var colors = [d3.rgb("#a6cee3"), d3.rgb("#1f78b4"), d3.rgb("#b2df8a"), d3.rgb("#33a02c"), d3.rgb("#fb9a99"), d3.rgb("#e31a1c"), d3.rgb("#fdbf6f"), d3.rgb("#ff7f00"), d3.rgb("#cab2d6"), d3.rgb("#6a3d9a")];
 
 d3.csv("data.csv", function(data) {
 	data.forEach(function(d) {
@@ -8,42 +12,17 @@ d3.csv("data.csv", function(data) {
 			var asdf = [];
 			dataset.push(asdf);
 		}
-		dataset[index].push({name:d.hero1, x:+d.h1x, y:+d.h1y});
-		dataset[index].push({name:d.hero2, x:+d.h2x, y:+d.h2y});
-		dataset[index].push({name:d.hero3, x:+d.h3x, y:+d.h3y});
-		dataset[index].push({name:d.hero4, x:+d.h4x, y:+d.h4y});
-		dataset[index].push({name:d.hero5, x:+d.h5x, y:+d.h5y});
-		dataset[index].push({name:d.hero6, x:+d.h6x, y:+d.h6y});
-		dataset[index].push({name:d.hero7, x:+d.h7x, y:+d.h7y});
-		dataset[index].push({name:d.hero8, x:+d.h8x, y:+d.h8y});
-		dataset[index].push({name:d.hero9, x:+d.h9x, y:+d.h9y});
-		dataset[index].push({name:d.hero10, x:+d.h10x, y:+d.h10y});
-		
-		/*
-		d.second = +d.second;
-		d.h1x = +d.h1x;
-		d.h1y = +d.h1y;
-		d.h2x = +d.h2x;
-		d.h2y = +d.h2y;
-		d.h3x = +d.h3x;
-		d.h3y = +d.h3y;
-		d.h4x = +d.h4x;
-		d.h4y = +d.h4y;
-		d.h5x = +d.h5x;
-		d.h5y = +d.h5y;
-		d.h6x = +d.h6x;
-		d.h6y = +d.h6y;
-		d.h7x = +d.h7x;
-		d.h7y = +d.h7y;
-		d.h8x = +d.h8x;
-		d.h8y = +d.h8y;
-		d.h9x = +d.h9x;
-		d.h9y = +d.h9y;
-		d.h10x = +d.h10x;
-		d.h10y = +d.h10y;
-		*/
+		dataset[index].push({id:1, name:d.hero1, x:+d.h1x, y:+d.h1y, team:+d.h1t, hp:+d.h1hp});
+		dataset[index].push({id:2, name:d.hero2, x:+d.h2x, y:+d.h2y, team:+d.h2t, hp:+d.h2hp});
+		dataset[index].push({id:3, name:d.hero3, x:+d.h3x, y:+d.h3y, team:+d.h3t, hp:+d.h3hp});
+		dataset[index].push({id:4, name:d.hero4, x:+d.h4x, y:+d.h4y, team:+d.h4t, hp:+d.h4hp});
+		dataset[index].push({id:5, name:d.hero5, x:+d.h5x, y:+d.h5y, team:+d.h5t, hp:+d.h5hp});
+		dataset[index].push({id:6, name:d.hero6, x:+d.h6x, y:+d.h6y, team:+d.h6t, hp:+d.h6hp});
+		dataset[index].push({id:7, name:d.hero7, x:+d.h7x, y:+d.h7y, team:+d.h7t, hp:+d.h7hp});
+		dataset[index].push({id:8, name:d.hero8, x:+d.h8x, y:+d.h8y, team:+d.h8t, hp:+d.h8hp});
+		dataset[index].push({id:9, name:d.hero9, x:+d.h9x, y:+d.h9y, team:+d.h9t, hp:+d.h9hp});
+		dataset[index].push({id:10, name:d.hero10, x:+d.h10x, y:+d.h10y, team:+d.h10t, hp:+d.h10hp});
 	});
-	//dataset = data.map(function(d) {return [ +d["h1x"], +d["h1y"] ]; }).filter(function(d) {return d[0]>0});
 	
 	document.getElementById("inputtime").max = dataset.length - 1;
 	update();
@@ -68,56 +47,72 @@ function update() {
 	var yMap = function(d) {return yScale(yValue(d));}
 	var yAxis = d3.svg.axis().scale(yScale).orient('left');
 	
-	var cValue = function(d) {return d.name;}
-	var color = d3.scale.category10();
+	var xMap2 = function(d) {return xScale(xValue(d)) - dotsize;}
+	var yMap2 = function(d) {return yScale(yValue(d)) - dotsize;}
+	
+	var color = function(d) {return colors[d.id-1];};
+	var stroke = function(d) {return ["", "", "green", "red"][d.team];}
 	
 	var svg = d3.select('.chart').append('svg')
 		.attr('width', width + margin.right + margin.left)
 		.attr('height', height + margin.top + margin.bottom)
 		.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 	
-	/*
-	svg.append('g')
-		.attr('transform', 'translate(0,' + height + ')')
-		.attr('class', 'x axis')
-		.call(xAxis);
+	var dots = svg.selectAll(".dot").data(data).enter()
 	
-	svg.append('g')
-		.attr('transform', 'translate(0,0)')
-		.attr('class', 'y axis')
-		.call(yAxis);
-	*/
-	
-	svg.selectAll(".dot")
-		.data(data)
-		.enter().append("circle")
+	dots.append("rect")
+		.filter(function(d) {return d.hp <= 0;})
 		.attr("class", "dot")
-		.attr("r", 10)
+		.attr("x", xMap2)
+		.attr("y", yMap2)
+		.attr("width", dotsize * 2)
+		.attr("height", dotsize * 2)
+		.style("fill", function(d) {return color(d);})
+		.style("stroke", function(d) {return stroke(d);})
+		.style("stroke-width", dotsize * 0.4);
+	
+	dots.append("circle")
+		.filter(function(d) {return d.hp > 0;})
+		.attr("class", "dot")
+		.attr("r", dotsize)
 		.attr("cx", xMap)
 		.attr("cy", yMap)
-		.style("fill", function(d) {return color(cValue(d));});
+		.style("fill", function(d) {return color(d);})
+		.style("stroke", function(d) {return stroke(d);})
+		.style("stroke-width", dotsize * 0.4);
 	
 	// draw legend
 	var legend = svg.selectAll(".legend")
-		.data(color.domain())
+		.data(data)
 		.enter().append("g")
 		.attr("class", "legend")
-		.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+		.attr("transform", function(d, i) { return "translate(0," + i * (legendsize * 2 + 6) + ")"; });
 
-	// draw legend colored rectangles
 	legend.append("rect")
+		.filter(function(d) {return d.hp <= 0;})
 		.attr("x", width - 70)
-		.attr("width", 16)
-		.attr("height", 16)
-		.style("fill", color);
+		.attr("width", legendsize * 2)
+		.attr("height", legendsize * 2)
+		.style("fill", color)
+		.style("stroke", function(d) {return stroke(d);})
+		.style("stroke-width", legendsize * 0.4);;
+	
+	legend.append("circle")
+		.filter(function(d) {return d.hp > 0;})
+		.attr("cx", width - 70 + legendsize)
+		.attr("cy", legendsize)
+		.attr("r", legendsize)
+		.style("fill", color)
+		.style("stroke", function(d) {return stroke(d);})
+		.style("stroke-width", legendsize * 0.4);;
 
 	// draw legend text
 	legend.append("text")
-		.attr("x", width - 50)
-		.attr("y", 9)
+		.attr("x", width - 65 + (legendsize * 2))
+		.attr("y", legendsize)
 		.attr("dy", ".35em")
 		.style("text-anchor", "start")
-		.text(function(d) { return d;})
+		.text(function(d) { return d.name;})
 };
 
 function updatetime() {
@@ -127,70 +122,3 @@ function updatetime() {
     d3.select("svg").remove();
     update();
 }
-
-/*
-	var margin = {top: 20, right: 15, bottom: 60, left: 60}
-	  , width = 960 - margin.left - margin.right
-	  , height = 500 - margin.top - margin.bottom;
-	
-	var x = d3.scale.linear()
-	          .domain([d3.min(data, function(d) { return d[0]; }),
-	                   d3.max(data, function(d) { return d[0]; })])
-	          .range([ 0, width ]);
-	
-	var y = d3.scale.linear()
-		      .domain([ d3.min(data, function(d) { return d[1]; }),
-	                  d3.max(data, function(d) { return d[1]; })])
-		      .range([ height, 0 ]);
-	
-	var chart = d3.select('body')
-	.append('svg:svg')
-	.attr('width', width + margin.right + margin.left)
-	.attr('height', height + margin.top + margin.bottom)
-	.attr('class', 'chart')
-	
-	var main = chart.append('g')
-	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-	.attr('width', width)
-	.attr('height', height)
-	.attr('class', 'main')
-	
-	// draw the x axis
-	var xAxis = d3.svg.axis()
-	.scale(x)
-	.orient('bottom');
-	
-	main.append('g')
-	.attr('transform', 'translate(0,' + height + ')')
-	.attr('class', 'main axis date')
-	.call(xAxis);
-	
-	// draw the y axis
-	var yAxis = d3.svg.axis()
-	.scale(y)
-	.orient('left');
-	
-	main.append('g')
-	.attr('transform', 'translate(0,0)')
-	.attr('class', 'main axis date')
-	.call(yAxis);
-	
-	var g = main.append("svg:g");
-	
-	svg.selectAll(".dot")
-		.data(data)
-		.enter().append("circle")
-		.attr("class", "dot")
-		.attr("r", 3.5)
-		.attr("cx", xMap)
-		.attr("cy", yMap)
-		.style("fill", function(d) { return color(cValue(d));});
-	
-	g.selectAll("scatter-dots")
-	  .data(data)
-	  .enter().append("svg:circle")
-	      .attr("cx", function (d,i) { return x(d[0]); } )
-	      .attr("cy", function (d) { return y(d[1]); } )
-	      .attr("r", 8);
-});
-*/
--- src/main/java/skadistats/clarity/examples/entitymovement/Main.java
+++ src/main/java/skadistats/clarity/examples/entitymovement/Main.java
@@ -20,7 +20,7 @@ public class Main {
     
     private BufferedWriter writer;
     private ArrayList<String> heroes;
-    private ArrayList< Hashtable<String, int[]> > locationspersecond;
+    private ArrayList< Hashtable<String, int[]> > datapersecond;
 
     @Insert
     private Context ctx;
@@ -34,37 +34,32 @@ public class Main {
         //if entity is a hero and has the location property
         if (e.hasProperty("m_cellX") && entityname.contains("DT_DOTA_Unit_Hero")) {
             String heroname = entityname.substring(18);
+            
             //keep a list of heroes in the match
             if (heroes.contains(heroname) != true) {
                 heroes.add(heroname);
             }
             
             //Create empty rows for each second, each row will contain location info for each hero, if it exists
-            while (second >= locationspersecond.size()) {
+            while (second >= datapersecond.size()) {
                 Hashtable<String, int[]> temp = new Hashtable<String, int[]>();
-                locationspersecond.add(temp);
+                datapersecond.add(temp);
             }
             
             //gather the info and put it in the array
             Integer xpos = e.getProperty("m_cellX");
             Integer ypos = e.getProperty("m_cellY");
-            int[] pos = new int[]{xpos, ypos};
-            locationspersecond.get(second).put(heroname, pos);
-            
-            /*
-            try {
-                writer.write(String.format("%d\t%s\t%d\t%d\n", tick, entityname.substring(18), xpos, ypos));
-            } catch (IOException exception) {
-                
-            }
-            */
+            Integer team = e.getProperty("m_iTeamNum");
+            Integer hp = e.getProperty("m_iHealth");
+            int[] data = new int[]{xpos, ypos, team, hp};
+            datapersecond.get(second).put(heroname, data);
         }
     }
 
     public void run(String[] args) throws Exception {
-        writer = new BufferedWriter(new FileWriter("output2.txt"));
+        writer = new BufferedWriter(new FileWriter("data.csv"));
         heroes = new ArrayList<String>();
-        locationspersecond = new ArrayList< Hashtable<String, int[]> >();
+        datapersecond = new ArrayList< Hashtable<String, int[]> >();
         
         long tStart = System.currentTimeMillis();
         SimpleRunner r = null;
@@ -78,21 +73,34 @@ public class Main {
             }
         }
         
-        for (int i = 0; i < locationspersecond.size(); i++) {
-            Hashtable<String, int[]> temp = locationspersecond.get(i);
+        //At the moment the api only returns data if the entity had a property change, so there are a bunch of "holes" in the data. This here attempts to fill those holes by using previous data.
+        Hashtable<String, int[]> prevdata = new Hashtable<String, int[]>();
+        
+        writer.write("second,hero1,h1x,h1y,h1t,h1hp,hero2,h2x,h2y,h2t,h2hp,hero3,h3x,h3y,h3t,h3hp,hero4,h4x,h4y,h4t,h4hp,hero5,h5x,h5y,h5t,h5hp,hero6,h6x,h6y,h6t,h6hp,hero7,h7x,h7y,h7t,h7hp,hero8,h8x,h8y,h8t,h8hp,hero9,h9x,h9y,h9t,h9hp,hero10,h10x,h10y,h10t,h10hp\n");
+        for (int i = 0; i < datapersecond.size(); i++) {
+            Hashtable<String, int[]> temp = datapersecond.get(i);
             String line = String.format("%d", i);
             
             for (int j = 0; j < heroes.size(); j++) {
                 String hero = heroes.get(j);
-                int[] pos;
+                
+                int[] data;
                 if (temp.get(hero) == null) {
-                    pos = new int[]{0,0};
+                    if (prevdata.containsKey(hero)) {
+                        data = prevdata.get(hero);
+                    }
+                    else {
+                        data = new int[]{0,0,0,0};
+                    }
                 }
                 else {
-                    pos = temp.get(hero);
+                    data = temp.get(hero);
+                    prevdata.put(hero, data);
                 }
-                line += String.format(",%s,%d,%d", hero, pos[0], pos[1]);
+                
+                line += String.format(",%s,%d,%d,%d,%d", hero, data[0], data[1], data[2], data[3]);
             }
+            
             line += "\n";
             writer.write(line);
         }
