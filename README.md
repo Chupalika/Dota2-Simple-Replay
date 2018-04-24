44bd5d49c3030874833d5d159e0cb0c7f80f8faa
Brandon Ip <Chupalika@gmail.com>
2018-04-23 21:42:24 -0400

    optimized trail drawing
    - drawing dots and trails is more efficient now, but requires a ~4
    second preload time of the page
    - trails can go up to max length now

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 A visualization tool to view simplified Dota 2 replays. Developed using [clarity-examples](https://github.com/skadistats/clarity-examples). This project was developed for a class project, so don't expect too much. :)
 
 # Changelog
+* [**2018-04-23**]
+  * Drawing dots and trails is more efficient now, but requires a ~4 second preload time of the page (need to implement loading text). Trails can go up to max length now.
+* [**2018-04-23**]
+  * Added trails behind each player, defaults to length of 5 seconds, can be adjusted with a slider from 0 to 20.
+  * The time is now displayed in mm:ss format.
 * [**2018-04-18**]
   * Dots are bordered green or red depending on team.
   * Dots no longer disappear if the player hasn't moved.
--- scatterchart.js
+++ scatterchart.js
@@ -1,10 +1,33 @@
 var dataset = [];
+var previoussecond = 0;
 var currentsecond = 0;
 var dotsize = 10;
 var legendsize = 10;
-var traillength = 10;
+var traillength = 5;
 //var colors = [d3.rgb("#9e0142"), d3.rgb("#d53e4f"), d3.rgb("#f46d43"), d3.rgb("#fdae61"), d3.rgb("#fee08b"), d3.rgb("#e6f598"), d3.rgb("#abdda4"), d3.rgb("#66c2a5"), d3.rgb("#3288bd"), d3.rgb("#5e4fa2")];
 var colors = [d3.rgb("#a6cee3"), d3.rgb("#1f78b4"), d3.rgb("#b2df8a"), d3.rgb("#33a02c"), d3.rgb("#fb9a99"), d3.rgb("#e31a1c"), d3.rgb("#fdbf6f"), d3.rgb("#ff7f00"), d3.rgb("#cab2d6"), d3.rgb("#6a3d9a")];
+
+var margin = {top: 40, right: 40, bottom: 40, left: 40};
+var width = 830 - margin.left - margin.right;
+var height = 700 - margin.top - margin.bottom;
+
+var xValue = function(d) {return d.x;};
+var xScale = d3.scale.linear().range([0, width-120]).domain([68, 185]);
+var xMap = function(d) {return xScale(xValue(d));}
+var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
+
+var yValue = function(d) {return d.y;};
+var yScale = d3.scale.linear().range([height, 0]).domain([65, 182]);
+var yMap = function(d) {return yScale(yValue(d));}
+var yAxis = d3.svg.axis().scale(yScale).orient('left');
+
+var xMap2 = function(d) {return xScale(xValue(d)) - dotsize;}
+var yMap2 = function(d) {return yScale(yValue(d)) - dotsize;}
+
+var color = function(d) {return colors[d.id-1];};
+var stroke = function(d) {return ["", "", "green", "red"][d.team];}
+
+var svg;
 
 window.onload = function() {
     document.getElementById("options").style.top = ((legendsize * 2 + 6) * 10) + 90 + "px";
@@ -31,97 +54,94 @@ d3.csv("data.csv", function(data) {
 	});
 	
 	document.getElementById("inputtime").max = dataset.length - 1;
+	document.getElementById("inputTL").max = dataset.length - 1;
+	continuesetup();
 	update();
 });
 
-function update() {
+function continuesetup() {
 	console.log(dataset);
 	
-	var data = dataset[currentsecond];
-	
-	var margin = {top: 40, right: 40, bottom: 40, left: 40};
-	var width = 830 - margin.left - margin.right;
-	var height = 700 - margin.top - margin.bottom;
-	
-	var xValue = function(d) {return d.x;};
-	var xScale = d3.scale.linear().range([0, width-120]).domain([68, 185]);
-	var xMap = function(d) {return xScale(xValue(d));}
-	var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
-	
-	var yValue = function(d) {return d.y;};
-	var yScale = d3.scale.linear().range([height, 0]).domain([65, 182]);
-	var yMap = function(d) {return yScale(yValue(d));}
-	var yAxis = d3.svg.axis().scale(yScale).orient('left');
-	
-	var xMap2 = function(d) {return xScale(xValue(d)) - dotsize;}
-	var yMap2 = function(d) {return yScale(yValue(d)) - dotsize;}
-	
-	var color = function(d) {return colors[d.id-1];};
-	var stroke = function(d) {return ["", "", "green", "red"][d.team];}
-	
-	var svg = d3.select('.chart').append('svg')
+	svg = d3.select('.chart').append('svg')
 		.attr('width', width + margin.right + margin.left)
 		.attr('height', height + margin.top + margin.bottom)
-		.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+		.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 	
-	var dots = svg.selectAll(".dot").data(data).enter();
+	for (i = 0; i < dataset.length - 1; i++) {
+		var data = dataset[i];
+		var dots = svg.selectAll(".dot" + i).data(data).enter();
 	
-	dots.append("rect")
-		.filter(function(d) {return d.hp <= 0;})
-		.attr("class", "dot")
-		.attr("x", xMap2)
-		.attr("y", yMap2)
-		.attr("width", dotsize * 2)
-		.attr("height", dotsize * 2)
-		.style("fill", function(d) {return color(d);})
-		.style("stroke", function(d) {return stroke(d);})
-		.style("stroke-width", dotsize * 0.4);
-		//.attr("transform", "rotate(45)");
+		dots.append("rect")
+			.filter(function(d) {return d.hp <= 0;})
+			.attr("id", "dot" + i)
+			.attr("heroid", function(d) {return d.id;})
+			.attr("class", "dot")
+			.attr("cx", xMap)
+			.attr("cy", yMap)
+			.attr("x", xMap2)
+			.attr("y", yMap2)
+			.attr("width", dotsize * 2)
+			.attr("height", dotsize * 2)
+			.style("fill", function(d) {return color(d);})
+			.style("stroke", function(d) {return stroke(d);})
+			.style("stroke-width", dotsize * 0.4)
+			.style("display", "none");
+			//.attr("transform", "rotate(45)");
+		
+		dots.append("circle")
+			.filter(function(d) {return d.hp > 0;})
+			.attr("id", "dot" + i)
+			.attr("heroid", function(d) {return d.id;})
+			.attr("class", "dot")
+			.attr("r", dotsize)
+			.attr("cx", xMap)
+			.attr("cy", yMap)
+			.style("fill", function(d) {return color(d);})
+			.style("stroke", function(d) {return stroke(d);})
+			.style("stroke-width", dotsize * 0.4)
+			.style("display", "none");
+	}
+}
+
+function update() {
+	var data = dataset[currentsecond];
 	
-	dots.append("circle")
-		.filter(function(d) {return d.hp > 0;})
-		.attr("class", "dot")
-		.attr("r", dotsize)
-		.attr("cx", xMap)
-		.attr("cy", yMap)
-		.style("fill", function(d) {return color(d);})
-		.style("stroke", function(d) {return stroke(d);})
-		.style("stroke-width", dotsize * 0.4);
+	svg.selectAll("#dot" + previoussecond)
+		.style("display", "none");
+	svg.selectAll("#dot" + currentsecond)
+		.style("display", "inline");
 	
+	d3.selectAll("path").remove();
 	//draw trails!
 	var stars = [[], [], [], [], [], [], [], [], [], []];
 	for (i = 0; i <= traillength; i++) {
-		if (currentsecond >= 1) {
-			var data2 = dataset[currentsecond-i];
-			var dots2 = svg.selectAll(".dot" + i).data(data2).enter();
-			data2.forEach(function(item, index) {stars[index].push(item);});
-			
-			dots2.append("circle")
-				.filter(function(d) {return d.hp > 0;})
-				.attr("class", "dot")
-				.attr("r", 2)
-				.attr("cx", xMap)
-				.attr("cy", yMap)
-				.style("fill", function(d) {return color(d);})
-				.style("stroke-width", 0);
-			
-		}
+		svg.selectAll("#dot" + (currentsecond - i)).each(function(item, index) {
+			if (d3.select(this).attr("cx") < 0 || d3.select(this).attr("cy") < 0) {
+				return;
+			}
+			stars[d3.select(this).attr("heroid")-1].push({x:d3.select(this).attr("cx"), y:d3.select(this).attr("cy"), color:d3.select(this).style("fill")});
+		});
 	}
 	
 	var link = d3.svg.line()
-		.x(xMap)
-		.y(yMap)
+		.x(function(d) {return d.x;})
+		.y(function(d) {return d.y;})
 		.interpolate("linear");
 	
-	stars.forEach(function(item, index) {
-		asdf = item[0];
-		svg.append("path")
-			.attr("d", link(item))
-			.attr("class", "link")
-			.style("stroke", color(asdf))
-			.style("stroke-width", 5);
-	});
+	if (traillength > 0) {
+		stars.forEach(function(item, index) {
+			if (item.length == 0) {
+				return;
+			}
+			svg.append("path")
+				.attr("d", link(item))
+				.attr("class", "link")
+				.style("stroke", item[0].color)
+				.style("stroke-width", 5);
+		});
+	}
 	
+	d3.selectAll(".legend").remove();
 	// draw legend
 	var legend = svg.selectAll(".legend")
 		.data(data)
@@ -157,16 +177,17 @@ function update() {
 };
 
 function updatetime() {
+    previoussecond = currentsecond;
     currentsecond = document.getElementById("inputtime").value;
     document.getElementById("minute").innerHTML = String(Math.floor(currentsecond/60)).padStart(2, "0");
     document.getElementById("second").innerHTML = String(currentsecond%60).padStart(2, "0");
-    d3.select("svg").remove();
+    //d3.select("svg").remove();
     update();
 }
 
 function updateTL() {
     traillength = document.getElementById("inputTL").value;
     document.getElementById("TL").innerHTML = traillength;
-    d3.select("svg").remove();
+    //d3.select("svg").remove();
     update();
 }
